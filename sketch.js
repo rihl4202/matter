2ef2686913094fec939d394481fbaa97574a3790
rihl4202 <88212189+rihl4202@users.noreply.github.com>
2021-08-26 11:11:52 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+var ball;
+var ground;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+
+  ground = Bodies.rectangle(200,390,400,20,ground_options);
+  World.add(world,ground);
+  wall = Bodies.rectangle(300,260,200,20,ground_options);
+  World.add(world,wall);
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  ball2 = Bodies.circle(320,15,10,ball_options)
+  World.add(world,ball2)
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  fill("green")
+  ellipse(ball.position.x,ball.position.y,20);
+  ellipse(ball2.position.x,ball2.position.y,10)
+  rect(ground.position.x,ground.position.y,400,20);
+  rect(wall.position.x,wall.position.y,200,20);
+
+}
+
